9cce75a4a7a1b19426422461dfa4339550b86747
artavasdes <56280139+artavasdes@users.noreply.github.com>
2021-06-09 08:22:43 -0400

    Add files via upload

---
+++ src/io/github/survivalPlugin/main/Main.java
@@ -0,0 +1,70 @@
+package io.github.minigameplugin.main;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		//startup
+		//reloads
+		//plugin reloads
+		//register events
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new JoinEvents(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		//shutdown
+		//reloads
+		//plugin reloads
+	}
+	
+	//reset
+	
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("reset")) {
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+				if(player.hasPermission("reset.use")) {
+					player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD+"Resetting Minigame!");	
+					//reset minigame
+					Reset.returnTp();
+					return true;
+				}
+				player.sendMessage(ChatColor.RED + "You do not have permission to use this command, contact a server mod!");
+				return true;
+			}
+			else {
+				//reset command for server
+				sender.sendMessage("Resetting minigame");
+				return true;
+			}
+		}
+		if(label.equalsIgnoreCase("start")) {
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+					player.sendMessage(ChatColor.DARK_GREEN + "" + ChatColor.BOLD+"Starting Game Soon!");
+					//start game
+					Start.teleport();
+					Start.countdown();
+					Start.tntDrop();
+					//TODO, add method for starting game
+					return true;
+			}
+			else {
+				//reset command for server
+				sender.sendMessage("Starting Game");
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/io/github/survivalPlugin/main/Start.java
@@ -0,0 +1,72 @@
+package io.github.minigameplugin.main;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+import org.bukkit.entity.Player;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.FallingBlock;
+
+
+public class Start {
+	// map boundaries TODO
+	static int x1 = 0;
+	static int x2 = 0;
+	static int z1 = 0;
+	static int z2 = 0;
+	static int y = 0;
+	
+	public static void teleport() {
+		//Teleports players to set locations
+		ArrayList<Location> locations = new ArrayList<Location>();
+		Player player = player.getPlayer();
+		World world = player.getWorld();
+		Location l = new Location(world,10,70,10);
+		locations.add(l);
+		Location l2 = new Location(world,15,70,10);
+		locations.add(l2);
+		Location l3 = new Location(world,20,70,10);
+		locations.add(l3);
+		Location l4 = new Location(world,25,70,10);
+		locations.add(l4);
+		Location l5 = new Location(world,30,70,10);
+		locations.add(l5);
+		int i = 0;
+		for(Player p : Bukkit.getOnlinePlayers()) {
+			p.teleport(locations.get(i));
+			i++;
+		}
+		
+	}
+	public static void countdown() throws InterruptedException {
+		//5 second countdown until tnt
+		Bukkit.broadcastMessage("Starting in 5...");
+		TimeUnit.SECONDS.sleep(1);
+		Bukkit.broadcastMessage("4...");
+		TimeUnit.SECONDS.sleep(1);
+		Bukkit.broadcastMessage("3...");
+		TimeUnit.SECONDS.sleep(1);
+		Bukkit.broadcastMessage("2...");
+		TimeUnit.SECONDS.sleep(1);
+		Bukkit.broadcastMessage("1...");
+		TimeUnit.SECONDS.sleep(1);
+		Bukkit.broadcastMessage("Start!");
+	}
+	
+	public static void tntDrop() {
+		World world = Bukkit.getWorld("world");
+		for (int i = x1; i <= x2; i++) {
+			for (int j = z1; j <= z2; j++) {
+				Location loc = new Location(world, i, y, j);
+				FallingBlock tnt = (FallingBlock) world.spawnFallingBlock(loc, Material.TNT.createBlockData());
+			}
+		}
+	}
+}
+
