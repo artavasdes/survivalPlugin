59dae7f3d3c3bd8c2006b263f03f86037e966161
Vincent Kim <vinceentk@gmail.com>
2021-06-10 08:15:31 -0400

    Queue

--- src/io/github/survivalPlugin/main/Queue.java
+++ src/io/github/survivalPlugin/main/Queue.java
@@ -1,7 +1,26 @@
 package io.github.minigameplugin.main;
 
+import java.util.ArrayList;
+
 import org.bukkit.event.Listener;
 
+import io.github.minigameplugin.main.Location;
+import io.github.minigameplugin.main.Player;
+import io.github.minigameplugin.main.Start;
+
 public class Queue implements Listener {
+	//If enough players in queue, run start commandsArrayList<Player> list = new ArrayList<Player>();
 	//If enough players in queue, run start commands
+	
+	public void glassCage(Player p) {
+		Location loc = new Location(p.getWorld(), x, y, z); // TODO; insert coords to glass cage
+		list.add(p);
+		p.teleport(loc);
+	}
+	
+	public void checkIfEnoughPlayers() throws InterruptedException {
+		if (list.size() >= 3) { // minimum player limit
+			Start.countdown();
+		}
+	}
 }
