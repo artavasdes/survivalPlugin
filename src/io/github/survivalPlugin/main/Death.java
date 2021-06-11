a2cd78191492a47dfa26894372531a12c4fd220b
Vincent Kim <vinceentk@gmail.com>
2021-06-11 09:47:27 -0400

    set gamemode to adventure and spectator

--- src/io/github/survivalPlugin/main/Death.java
+++ src/io/github/survivalPlugin/main/Death.java
@@ -18,6 +18,7 @@ public class Death implements Listener {
             if (e.getDamage() >= p.getHealth()) {
             	Start.alive.remove(p);
             	Win.checkForWin();
+            	p.setGameMode("spectator");
             }
         }
 	}
